4979b4be8b78e3be446c247704826403ae869e5d
trinhleo <hnkt2907@gmail.com>
2017-05-13 17:56:51 +0700

    cp

--- app/components/control/control.controller.js
+++ app/components/control/control.controller.js
@@ -16,18 +16,24 @@
         vm.boiler_2 = false;
         vm.fan_1 = false;
         vm.fan_2 = false;
-        vm.light = false
+        vm.light_1 = false;
+        vm.light_2 = false;
         vm.fan1Change = fan1Change;
         vm.fan2Change = fan2Change;
         vm.pumb1Change = pumb1Change;
         vm.pumb2Change = pumb2Change;
         vm.boiler1Change = boiler1Change;
         vm.boiler2Change = boiler2Change;
-        vm.lightChange = lightChange;
-        vm.modeChange = modeChange;
-        vm.connected = false;
-        vm.wait = false;
-        vm.socket = io.connect('https://hoanghuuhung.herokuapp.com/device-node');
+        vm.light1Change = light1Change;
+        vm.mode1Change = mode1Change;
+        vm.light2Change = light2Change;
+        vm.mode2Change = mode2Change;
+        vm.connected1 = false;
+        vm.wait1 = false;
+        vm.connected2 = false;
+        vm.wait2 = false
+        // vm.socket = io.connect('https://hoanghuuhung.herokuapp.com/device-node');
+        vm.socket = io.connect('http://localhost:3000/device-node');
         var config = {
             "org": "8usbvc",
             "id": "myapp",
@@ -97,70 +103,99 @@
                 console.log('wait...')
             }
         }
-        function lightChange() {
-            if (!vm.wait) {
-                vm.light = !vm.light;
-                console.log('light', vm.light);
-                vm.wait = true;
-                vm.socket.emit('light', vm.light);
+        function light1Change() {
+            if (!vm.wait1) {
+                vm.light_1 = !vm.light_1;
+                console.log('light1', vm.light_1);
+                vm.wait1 = true;
+                vm.socket.emit('light1', vm.light_1);
             } else {
                 console.log('wait...')
             }
         }
 
-        function modeChange() {
-            if (!vm.wait) {
-                vm.manual = !vm.manual;
-                console.log('mode', vm.manual);
-                vm.wait = true;
-                vm.socket.emit('mode', vm.manual);
+        function mode1Change() {
+            if (!vm.wait1) {
+                vm.manual_1 = !vm.manual_1;
+                console.log('mode1', vm.manual_1);
+                vm.wait1 = true;
+                vm.socket.emit('mode1', vm.manual_1);
             } else {
                 console.log('wait...')
             }
         }
 
-        vm.socket.on('deviceNodeData', function (data) {
-            vm.connected = true;
+        function light2Change() {
+            if (!vm.wait2) {
+                vm.light_2 = !vm.light_2;
+                console.log('light2', vm.light_2);
+                vm.wait2 = true;
+                vm.socket.emit('light2', vm.light_2);
+            } else {
+                console.log('wait...')
+            }
+        }
+
+        function mode2Change() {
+            if (!vm.wait2) {
+                vm.manual_2 = !vm.manual_2;
+                console.log('mode1', vm.manual_2);
+                vm.wait2 = true;
+                vm.socket.emit('mode1', vm.manual_2);
+            } else {
+                console.log('wait...')
+            }
+        }
+        vm.socket.on('deviceNodeData1', function (_data) {
+            var data = _data
+            vm.connected1 = true;
+            vm.wait1 = false;
             $timeout(function () {
                 if (data) {
-                    var pay = data
-                    console.log("pay: ", pay);
-                    if (pay.indexOf(";") > -1) {
-                        var array = pay.split(";");
-                        var newArray = [];
-                        console.log(array)
-                        vm.zone_1 = array[0].split(",")
-                        vm.zone_2 = array[1].split(",")
-                        if (vm.zone_1[3] === "on" || vm.zone_1[3] === "off") {
-                            vm.fan_1 = vm.zone_1[3] === "on" ? true : false;
-                        }
-                        if (vm.zone_2[3] === "on" || vm.zone_2[3] === "off") {
-                            vm.fan_2 = vm.zone_2[3] === "on" ? true : false;
-                        }
-                        if (vm.zone_1[4] === "on" || vm.zone_1[4] === "off") {
-                            vm.pumb_1 = vm.zone_1[4] === "on" ? true : false;
-                        }
-                        if (vm.zone_2[4] === "on" || vm.zone_2[4] === "off") {
-                            vm.pumb_2 = vm.zone_2[4] === "on" ? true : false;
-                        }
-                        if (vm.zone_1[5] === "on" || vm.zone_1[5] === "off") {
-                            vm.light = vm.zone_1[5] === "on" ? true : false;
-                        }
-                        if (vm.zone_1[6] === "on" || vm.zone_1[6] === "off") {
-                            vm.boiler_1 = vm.zone_1[6] === "on" ? true : false;
-                        }
-                        if (vm.zone_2[6] === "on" || vm.zone_2[6] === "off") {
-                            vm.boiler_2 = vm.zone_2[6] === "on" ? true : false;
-                        }
-                        console.log(vm.zone_1);
-                        console.log(vm.zone_2)
-                    } else {
-                        if(o){
-                        vm.zone_1 = o.split(",")
-                        }
+                    vm.zone_1 = data.split(",")
+                    if (vm.zone_1[4] === "on" || vm.zone_1[4] === "off") {
+                        vm.fan_1 = vm.zone_1[3] === "on" ? true : false;
                     }
+                    if (vm.zone_1[5] === "on" || vm.zone_1[5] === "off") {
+                        vm.pumb_1 = vm.zone_1[4] === "on" ? true : false;
+                    }
+                    if (vm.zone_1[3] === "on" || vm.zone_1[3] === "off") {
+                        vm.light_1 = vm.zone_1[5] === "on" ? true : false;
+                    }
+                    if (vm.zone_1[6] === "on" || vm.zone_1[6] === "off") {
+                        vm.boiler_1 = vm.zone_1[6] === "on" ? true : false;
+                    }
+                    if (vm.zone_1[7] === 0 || vm.zone_1[7] === 1) {
+                        vm.manual_1 = vm.zone_1[7] === 1 ? true : false;
+                    }
+                    console.log(vm.zone_1);
                 }
-
+            })
+        })
+        vm.socket.on('deviceNodeData2', function (_data) {
+            var data = _data
+            vm.connected2 = true;
+            vm.wait2 = false;
+            $timeout(function () {
+                if (data) {
+                    vm.zone_2 = data.split(",")
+                    if (vm.zone_2[4] === "on" || vm.zone_2[4] === "off") {
+                        vm.fan_2 = vm.zone_2[3] === "on" ? true : false;
+                    }
+                    if (vm.zone_2[5] === "on" || vm.zone_2[5] === "off") {
+                        vm.pumb_2 = vm.zone_2[4] === "on" ? true : false;
+                    }
+                    if (vm.zone_2[3] === "on" || vm.zone_2[3] === "off") {
+                        vm.light_2 = vm.zone_2[5] === "on" ? true : false;
+                    }
+                    if (vm.zone_2[6] === "on" || vm.zone_2[6] === "off") {
+                        vm.boiler_2 = vm.zone_2[6] === "on" ? true : false;
+                    }
+                    if (vm.zone_2[7] === "on" || vm.zone_7[7] === "off") {
+                        vm.manual_2 = vm.zone_2[7] === "on" ? true : false;
+                    }
+                    console.log(vm.zone_2);
+                }
             })
         })
     }
