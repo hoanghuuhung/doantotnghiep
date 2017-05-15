d8df97c659b9bfc2229a5662e7dff5836ac1bdfd
trinhleo <hnkt2907@gmail.com>
2017-05-15 23:16:16 +0700

    change account

--- app/components/control/control.controller.js
+++ app/components/control/control.controller.js
@@ -35,15 +35,15 @@
         var topicPublish1 = "event1";
         var topicPublish2 = "event2";
         // var client = new Paho.MQTT.Client("host", port, "client_id");
-        var client = new Paho.MQTT.Client("m13.cloudmqtt.com", 33492, "web_" + parseInt(Math.random() * 100, 10));
+        var client = new Paho.MQTT.Client("m20.cloudmqtt.com", 32733, "web_" + parseInt(Math.random() * 100, 10));
 
         // set callback handlers
         client.onConnectionLost = onConnectionLost;
         client.onMessageArrived = onMessageArrived;
         var options = {
             useSSL: true,
-            userName: "rauzdzhf",
-            password: "VXloiMpGvtxe",
+            userName: "ctcjmuyy",
+            password: "NFFKErc6QcQt",
             onSuccess: onConnect,
             onFailure: doFail
         }
@@ -95,13 +95,13 @@
                     if (data) {
                         vm.zone_1 = data.split(",")
                         if (vm.zone_1[4] === "on" || vm.zone_1[4] === "off") {
-                            vm.fan_1 = vm.zone_1[3] === "on" ? true : false;
+                            vm.fan_1 = vm.zone_1[4] === "on" ? true : false;
                         }
                         if (vm.zone_1[5] === "on" || vm.zone_1[5] === "off") {
-                            vm.pumb_1 = vm.zone_1[4] === "on" ? true : false;
+                            vm.pumb_1 = vm.zone_1[5] === "on" ? true : false;
                         }
                         if (vm.zone_1[3] === "on" || vm.zone_1[3] === "off") {
-                            vm.light_1 = vm.zone_1[5] === "on" ? true : false;
+                            vm.light_1 = vm.zone_1[3] === "on" ? true : false;
                         }
                         if (vm.zone_1[6] === "on" || vm.zone_1[6] === "off") {
                             vm.boiler_1 = vm.zone_1[6] === "on" ? true : false;
@@ -120,13 +120,13 @@
                     if (data) {
                         vm.zone_2 = data.split(",")
                         if (vm.zone_2[4] === "on" || vm.zone_2[4] === "off") {
-                            vm.fan_2 = vm.zone_2[3] === "on" ? true : false;
+                            vm.fan_2 = vm.zone_2[4] === "on" ? true : false;
                         }
                         if (vm.zone_2[5] === "on" || vm.zone_2[5] === "off") {
-                            vm.pumb_2 = vm.zone_2[4] === "on" ? true : false;
+                            vm.pumb_2 = vm.zone_2[5] === "on" ? true : false;
                         }
                         if (vm.zone_2[3] === "on" || vm.zone_2[3] === "off") {
-                            vm.light_2 = vm.zone_2[5] === "on" ? true : false;
+                            vm.light_2 = vm.zone_2[3] === "on" ? true : false;
                         }
                         if (vm.zone_2[6] === "on" || vm.zone_2[6] === "off") {
                             vm.boiler_2 = vm.zone_2[6] === "on" ? true : false;
